c300a16c3d17c77b24b680ed3375ce1a666510bc
yumichoi <yumichoi0921@gmail.com>
2021-05-01 21:56:47 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
+
 const mongoose = require("mongoose")
 mongoose.connect('mongodb+srv://yumichoi:gozldsstq533@boilerplate.eagki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -10,7 +20,26 @@ mongoose.connect('mongodb+srv://yumichoi:gozldsstq533@boilerplate.eagki.mongodb.
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요')
+})
+
+
+app.post('/register', (req, res) => {
+
+
+  
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다. 
+  
+  
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
